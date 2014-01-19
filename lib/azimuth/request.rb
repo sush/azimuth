44aa94dc5a8854db6970dea7061f29c3ee245843
sush <aylic.petit@gmail.com>
2014-01-19 20:51:23 +0100

    Allow underscores options keys

--- lib/azimuth/request.rb
+++ lib/azimuth/request.rb
@@ -1,10 +1,12 @@
+require 'active_support/core_ext/string/inflections'
+
 module Azimuth
   # Methods for HTTP requests
   module Request
     # Make a HTTP POST request.
     #
     # @param path [String] The path, relative to api_endpoint
-    # @param options [Hash] body params for request
+    # @param params [Hash] body params for request
     # @return [Hashie::Mash]
     def post(path, options={})
       request(:post, path, options)
@@ -12,7 +14,9 @@ module Azimuth
 
     private
 
-    def request(method, path, options)
+    def request(method, path, params)
+      camelize_options_keys(params[:options])
+
       response = connection.send(method) do |request|
         request.params['key'] = self.api_key
 
@@ -20,11 +24,21 @@ module Azimuth
         when :post
           request.headers['Content-Type'] = 'application/json'
           request.path = path
-          request.body = MultiJson.dump(options) unless options.empty?
+          request.body = MultiJson.dump(params) unless params.empty?
         end
       end
 
       response.body
+    end
+
+    def camelize_options_keys(options)
+      return unless options
+
+      camelized_options = options.inject({}) do |opt, v|
+        opt.merge!(v.first.to_s.camelize(:lower) => v.last)
+      end
+
+      options.replace(camelized_options)
     end
   end
 end
